9c9a48bfcfbbe66d7a75b9d542045e5cc94465fb
İsmail Hasır <ismailhasir@gmail.com>
2022-07-09 19:05:35 +0300

    restart

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React, { useState } from "react";
-import "./App.css";
-import List from "./components/List";
-
-interface IState {
-  people: {
-    name: string;
-    age: number;
-    url: string;
-    note: string;
-  }[];
-}
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [people, setPeople] = useState<IState["people"]>([]);
-
   return (
     <div className="App">
-      <h1>People Invited to Our Party</h1>
-      <List people={people} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
